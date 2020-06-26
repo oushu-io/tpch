6c4a20eab6244dc18e6971132f062635b65f2a56
oushu1shiheng1 <49219792+oushu1shiheng1@users.noreply.github.com>
2020-06-27 02:49:53 +0800

    Add files via upload

---
+++ sel.sql
@@ -0,0 +1,8 @@
+select count(1) from customer ;
+select count(1) from lineitem ;
+select count(1) from nation   ;
+select count(1) from orders   ;
+select count(1) from part     ;
+select count(1) from partsupp ;
+select count(1) from region   ;
+select count(1) from supplier ;
